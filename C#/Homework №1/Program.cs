7f1328b6e89cd732cc3ae878c589f3975c60f4c1
Владислав Морев <morev.vlad7@gmail.com>
2024-07-28 19:06:29 +0300

    C# Homeworks №1 and №2

---
+++ C#/Homework №1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace С__Homework__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Переменные, общие для некоторых заданий
+            string enteringString;
+
+            Console.WriteLine("Задание №1");
+
+            string numberString;
+            Console.Write("Enter your number: ");
+            numberString = Console.ReadLine();
+            int number = Convert.ToInt32(numberString);
+            if (number < 1 || number > 100)
+                Console.WriteLine("Error!");
+            else if (number % 3 == 0 && number % 5 == 0)
+                Console.WriteLine("Fizz Buzz!");
+            else if (number % 3 == 0)
+                Console.WriteLine("Fizz!");
+            else if (number % 5 == 0)
+                Console.WriteLine("Buzz!");
+            else
+                Console.WriteLine(number);
+
+            Console.WriteLine("--------------\n");
+
+            Console.WriteLine("Задание №2");
+
+            double numberP, percent;
+
+            Console.Write("Enter your number: ");
+            enteringString = Console.ReadLine();
+            numberP = Convert.ToDouble(enteringString);
+            Console.WriteLine();
+
+            Console.Write("Enter percent: ");
+            enteringString = Console.ReadLine();
+            percent = Convert.ToDouble(enteringString);
+            Console.WriteLine();
+
+            Console.Write("Answer: " + (numberP / 100) * percent);
+            
+            Console.WriteLine("\n--------------\n");
+
+            Console.WriteLine("Задание №3");
+
+            int num1, num2, num3, num4;
+
+            Console.Write("Enter number 1: ");
+            enteringString = Console.ReadLine();
+            num1 = Convert.ToInt32(enteringString);
+            
+            Console.Write("Enter number 2: ");
+            enteringString = Console.ReadLine();
+            num2 = Convert.ToInt32(enteringString);
+            
+            Console.Write("Enter number 3: ");
+            enteringString = Console.ReadLine();
+            num3 = Convert.ToInt32(enteringString);
+            
+            Console.Write("Enter number 4: ");
+            enteringString = Console.ReadLine();
+            num4 = Convert.ToInt32(enteringString);
+            Console.WriteLine();
+
+            Console.Write("Answer: " + ((num1*1000) + (num2 * 100) + (num3 * 10) + num4));
+
+            Console.WriteLine("\n--------------\n");
+
+            Console.WriteLine("Задание №6");
+
+            string choise;
+            double temperature;
+
+            Console.Write("Enter temperature: ");
+            enteringString = Console.ReadLine();
+            temperature = Convert.ToDouble(enteringString);
+
+            Console.Write("Your choise: ");
+            choise = Console.ReadLine();
+
+            if (choise == "f")
+                Console.WriteLine("Entered temperature is in Celsium. In Fahrenheit it will be: " + ((1.8 * temperature) + 32));
+            else if (choise == "c")
+                Console.WriteLine("Entered temperature is in Fahrenheit. In Celsium it will be: " + ((temperature - 32) / 1.8));
+            else
+                Console.WriteLine("Error! Enter right choise: 'f' or 'c'");
+
+            Console.WriteLine("\n--------------\n");
+
+            Console.WriteLine("Задание №7");
+
+            int start, end, temp;
+
+            Console.Write("Enter start of the range: ");
+            enteringString = Console.ReadLine();
+            start = Convert.ToInt32(enteringString);
+
+            Console.Write("Enter end of the range: ");
+            enteringString = Console.ReadLine();
+            end = Convert.ToInt32(enteringString);
+
+            if (start > end)
+            {
+                temp = start;
+                start = end;
+                end = temp;
+                Console.WriteLine("Normalisation has been done.");
+            }
+
+            Console.Write("All even numbers:");
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Homework №2/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Homework__2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание №1");
+
+            double[] A = new double[5];
+            double[] B = new double[5];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write("Enter number: ");
+                A[i] = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            PrintArr("First array: ", A);
+            FillRandom(B);
+            PrintArr("Second array: ", B);
+            Console.WriteLine();
+
+            Console.WriteLine("Common MAX element: " + CommonMax(A, B));
+            Console.WriteLine("Common MIN element: " + CommonMin(A, B));
+            Console.WriteLine("Common Sum of two arrays: " + CommonSum(A, B));
+            Console.WriteLine("Common Multiply of two arrays: " + CommonMultiply(A, B));
+            Console.WriteLine("Sum of all even elements of the first array: " + EvenSum(A));
+            Console.WriteLine("Sum of all odd elements of the second array: " + OddSum(B));
+
+            Console.ReadKey();
+
+            Console.WriteLine("------------\n");
+
+            Console.WriteLine("Задание №2");
+
+            int[,] array2D = new int[5, 5];
+            int[] array1D = new int[25];
+            int max, min, iMax = 0, iMin = 0, sum = 0;
+
+            Random random = new Random();
+
+            FillRandom2D(array2D);
+            PrintArr2D("array Z: ", array2D);
+            Console.WriteLine();
+
+            // Переводим в одномерный массив, чтобы легче было посчитать
+            array1D = Change(array2D, array1D);
+            
+            for (int i = 0; i < array1D.Length; i++)
+            {
+                if (array1D[iMax] < array1D[i])
+                {
+                    iMax = i;
+                }
+                
+                if (array1D[iMin] > array1D[i])
+                {
+                    iMin = i;
+                }
+            }
+
+            max = MaxArr(array1D);
+            min = MinArr(array1D);
+            Console.WriteLine("MAX: " + max);
+            Console.WriteLine("MIN: " + min + '\n');
+
+            if (iMin < iMax) 
+            {
+                for (int i = iMin + 1; i < iMax; i++)
+                    sum = sum + array1D[i];
+            }
+            else
+            {
+                for (int i = iMax + 1; i < iMin; i++)
+                    sum = sum + array1D[i];
+            }
+
+            Console.WriteLine("Summ: " + sum);
+
+            Console.ReadLine();
+        }
+        static void FillRandom(double[] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Math.Round(random.Next(0, 11) + random.NextDouble(), 1);
+            }
+        }
+        static void FillRandom2D(int[,] arr)
+        {
+            Random random = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    arr[i, j] = random.Next(-20, 21);
+            }
+        }
+        static void PrintArr(string text, double[] arr)
+        {
+            Console.Write(text + ": ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintArr2D(string text, int[,] arr)
+        {
+            Console.WriteLine(text + ": ");
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    Console.Write(arr[i, j] + " ");
+
+                Console.WriteLine();
+            }
+        }
+        static double CommonMax(double[] arr1, double[] arr2)
+        {
+            double max1 = double.MinValue, max2 = double.MinValue, commonMax;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] > max1)
+                    max1 = arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr2[i] > max2)
+                    max2 = arr2[i];
+            }
+
+            if (max1 > max2)
+                commonMax = max1;
+            else
+                commonMax = max2;
+
+            return commonMax;
+        }
+        static double CommonMin(double[] arr1, double[] arr2)
+        {
+            double min1 = double.MaxValue, min2 = double.MaxValue, commonMin;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if (arr1[i] < min1)
+                    min1 = arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                if (arr2[i] < min2)
+                    min2 = arr2[i];
+            }
+
+            if (min1 < min2)
+                commonMin = min1;
+            else
+                commonMin = min2;
+
+            return commonMin;
+        }
+        static double CommonSum(double[] arr1, double[] arr2)
+        {
+            double sum1 = 0, sum2 = 0, commonSum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                sum1 += arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                sum2 += arr2[i];
+            }
+
+            commonSum = sum1 + sum2;
+
+            return commonSum;
+        }
+        static double CommonMultiply(double[] arr1, double[] arr2)
+        {
+            double mult1 = 1, mult2 = 1, commonMult = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                mult1 *= arr1[i];
+            }
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                mult2 *= arr2[i];
+            }
+
+            commonMult = mult1 * mult2;
+
+            return commonMult;
+        }
+        static double EvenSum(double[] arr)
+        {
+            double evenSum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                    evenSum += arr[i];
+            }
+
+            return evenSum;
+        }
+        static double OddSum(double[] arr)
+        {
+            double oddSum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 != 0)
+                    oddSum += arr[i];
+            }
+
+            return oddSum;
+        }
+        static int MaxArr(int[] arr)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                max = max < arr[i] ? arr[i] : max;
+            }
+            return max;
+        }
+        static int MinArr(int[] arr)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                min = min > arr[i] ? arr[i] : min;
+            }
+            return min;
+        }
+        static int[] Change(int[,] arr1, int[] arr2)
+        {
+            int z = 0;
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    arr2[z] = arr1[i, j];
+                    z++;
+                }
+            return arr2;
+        }
+    }
+}
