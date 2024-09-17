207d52c5bc797200a628874e53096abeb88b8b1b
Владислав Морев <morev.vlad7@gmail.com>
2024-09-17 21:05:36 +0300

    Homework №4 (от 13.09.2024)

---
+++ WPF/Homework №4/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Lesson_4
+{
+    public partial class MainWindow : Window
+    {   
+        Random rnd;
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            rnd = new Random();
+            Color randomColor = Color.FromArgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256));
+            textBlock.Background = new SolidColorBrush(randomColor);
+        }
+    }
+}
