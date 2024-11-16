f8ed5cd5e3d4062c1b49bf51211a30333acba06e
Владислав Морев <morev.vlad7@gmail.com>
2024-11-16 16:18:38 +0300

    Homework №1 (Vegies And Fruits)

---
+++ C#-SQL/Homework №1 (Vegies And Fruits)/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace C__SQL_Homework__1__Vegies_and_Fruits_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                string ConnectionString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+                conn = new SqlConnection(ConnectionString);
+
+                conn.Open();
+                if (conn.State == ConnectionState.Open)
+                    Console.WriteLine("Connected to Database");
+
+                VFRepository vfRepo = new VFRepository(conn);
+
+                // Задание 3.1 - отображение всей информации из таблицы с овощами и фруктами
+
+                //var allVF = vfRepo.GetAll();
+
+                //Console.WriteLine(allVF);
+
+                // Задание 3.2 - отображение всех названий овощей и фруктов
+
+                //var allTitles = vfRepo.GetAllTitles();
+
+                //Console.WriteLine(allTitles);
+
+                // Задание 3.3 - отображение всех цветов
+
+                //var allColors = vfRepo.GetAllСolors();
+
+                //Console.WriteLine(allColors);
+
+                // Задание 3.4 - отображение максимальной калорийности
+
+                //var maxCalories = vfRepo.GetMaxCalories();
+
+                //Console.WriteLine(maxCalories); 
+
+                // Задание 3.4 - отображение минимальной калорийности
+
+                //var minCalories = vfRepo.GetMinCalories();
+
+                //Console.WriteLine(minCalories);
+
+                // Задание 3.4 - отображение минимальной калорийности
+
+                //var avgCalories = vfRepo.GetAvgCalories();
+
+                //Console.WriteLine(avgCalories);
+
+                // Задание 4.1 - показать количество овощей;
+
+                //var countVegies = vfRepo.GetVegiesCount();
+
+                //Console.WriteLine(countVegies);
+
+                // Задание 4.2 - показать количество фруктов;
+
+                //var countFruits = vfRepo.GetFruitsCount();
+
+                //Console.WriteLine(countFruits);
+
+                // Задание 4.3 - Показать количество овощей и фруктов заданного цвета;
+
+                //var countColor = vfRepo.GetByColor();
+
+                //Console.WriteLine(countColor);
+
+                // Задание 4.4 - Показать количество овощей и фруктов каждого цвета;
+
+                //var countEachColor = vfRepo.GetEachColor();
+
+                //Console.WriteLine(countEachColor);
+
+                // Задание 4.5 - Показать овощи и фрукты с калорийностью ниже указанной;
+
+                //var lessThan = vfRepo.GetCaloriesLess();
+
+                //Console.WriteLine(lessThan);
+
+                // Задание 4.6 - Показать овощи и фрукты с калорийностью выше указанной;
+
+                //var moreThan = vfRepo.GetCaloriesMore();
+
+                //Console.WriteLine(moreThan); 
+
+                // Задание 4.7 - Показать овощи и фрукты с калорийностью в указанном диапазоне;
+
+                //var range = vfRepo.GetCaloriesInRange();
+
+                //Console.WriteLine(range);
+
+                // Задание 4.8 - Показать все овощи и фрукты, у которых цвет желтый или красный;
+
+                //var redYellow = vfRepo.GetRedOrYellow();
+
+                //Console.WriteLine(redYellow);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+            }
+        }
+    }
+}
---
+++ C#-SQL/Homework №1 (Vegies And Fruits)/VFRepository.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__SQL_Homework__1__Vegies_and_Fruits_
+{
+    public class VFRepository
+    {
+        private readonly SqlConnection conn;
+
+        public VFRepository(SqlConnection connection)
+        {
+            conn = connection;
+        }
+
+        public string GetAll()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT * FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                for (int i = 0; i < rdr.FieldCount; i++)
+                {
+                    allVFData += rdr.GetName(i) + "\t";
+                }
+                allVFData += "\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr["Id"] + "\t" 
+                        + rdr["Title"] + "\t" + rdr["Type"] 
+                        + " \t" + rdr["Color"] + "\t" + rdr["Calories"] + "\n";
+                }
+            }
+            return allVFData;
+        }
+        public string GetAllTitles()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT Title FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "\t" + rdr.GetName(0);
+
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += "\t" + rdr["Title"] + "\n";
+                }
+            }
+            return allVFData;
+        }
+        public string GetAllСolors()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT Color FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "\t" + rdr.GetName(0);
+
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += "\t" + rdr["Color"] + "\n";
+                }
+            }
+            return allVFData;
+        }
+        public string GetMaxCalories()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT MAX(Calories) FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Max Calories: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+            return allVFData;
+        }
+        public string GetMinCalories()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT MIN(Calories) FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Min Calories: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+            return allVFData;
+        }
+        public string GetAvgCalories()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT AVG(Calories) FROM [Vegies&Fruits]";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Avg Calories: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+            return allVFData;
+        }
+        public string GetVegiesCount()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT COUNT(*) FROM [Vegies&Fruits] WHERE Type = 'Vegie'";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Vegies count: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+            
+            return allVFData;
+        }
+        public string GetFruitsCount()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT COUNT(*) FROM [Vegies&Fruits] WHERE Type = 'Fruit'";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Fruits count: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+            
+            return allVFData;
+        }
+        public string GetByColor()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT COUNT(*) FROM [Vegies&Fruits] WHERE Color = 'Red'";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += " " + "Red vegies and fruits count: ";
+
+                if (rdr.Read())
+                    allVFData += "\t" + rdr[0];
+            }
+
+            return allVFData;
+        }
+        
+        public string GetEachColor()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT Color, COUNT(*) FROM [Vegies&Fruits] GROUP BY Color;";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                
+                allVFData += "Color" + "\t" + "Count";
+                
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr[0] + "\t"
+                        + rdr[1] + "\n";
+                }
+            }
+
+            return allVFData;
+        }
+        public string GetCaloriesLess()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT * FROM [Vegies&Fruits] WHERE Calories < (SELECT AVG(Calories) FROM [Vegies&Fruits]);";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "Id" + "\t" + "Title" + "\t" + "Calories";
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr["Id"] + "\t"
+                        + rdr["Title"] + "    " + rdr["Calories"] + "\n";
+                }
+            }
+
+            return allVFData;
+        } 
+        
+        public string GetCaloriesMore()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT * FROM [Vegies&Fruits] WHERE Calories > (SELECT AVG(Calories) FROM [Vegies&Fruits]);";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "Id" + "\t" + "Title" + "\t" + "Calories";
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr["Id"] + "\t"
+                        + rdr["Title"] + "    " + rdr["Calories"] + "\n";
+                }
+            }
+
+            return allVFData;
+        }
+        
+        public string GetCaloriesInRange()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT * FROM [Vegies&Fruits] WHERE Calories BETWEEN 40 AND 60";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "Id" + "\t" + "Title" + "\t" + "Calories";
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr["Id"] + "\t"
+                        + rdr["Title"] + "    " + rdr["Calories"] + "\n";
+                }
+            }
+
+            return allVFData;
+        }
+        public string GetRedOrYellow()
+        {
+            string allVFData = "";
+
+            string selectString = "SELECT * FROM [Vegies&Fruits] WHERE Color IN ('Red', 'Yellow')";
+            SqlCommand cmd = new SqlCommand(selectString, conn);
+
+            using (SqlDataReader rdr = cmd.ExecuteReader())
+            {
+                allVFData += "Id" + "\t" + "Title" + "\t" + "Color";
+                allVFData += "\n\n";
+
+                while (rdr.Read())
+                {
+                    allVFData += rdr["Id"] + "\t"
+                        + rdr["Title"] + "    " + rdr["Color"] + "\n";
+                }
+            }
+
+            return allVFData;
+        }
+    }
+}
