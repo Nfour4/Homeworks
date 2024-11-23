07a29985242cdf5d6157ff03fe7325776abb55a6
Владислав Морев <morev.vlad7@gmail.com>
2024-11-23 18:09:03 +0300

    Homework №3 (Disconnected Mode OfficeSupplies)

---
+++ C#-SQL/Homework №3 (Disconnected Mode OfficeSupplies)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Homework__3__Disconnected_mode_WPF_
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
